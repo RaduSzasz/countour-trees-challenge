f80330e0d6e962a357bc3164fe3e30c66a1a9696
Radu Szasz <radu.szasz@gmail.com>
2017-02-26 00:02:25 +0000

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/tourtre_simple.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/radu/Programming/ContourTrees/tourtre-simple/src/ctArc.c" "/home/radu/Programming/ContourTrees/tourtre-simple/cmake-build-debug/CMakeFiles/tourtre_simple.dir/src/ctArc.c.o"
+  "/home/radu/Programming/ContourTrees/tourtre-simple/src/ctBranch.c" "/home/radu/Programming/ContourTrees/tourtre-simple/cmake-build-debug/CMakeFiles/tourtre_simple.dir/src/ctBranch.c.o"
+  "/home/radu/Programming/ContourTrees/tourtre-simple/src/ctComponent.c" "/home/radu/Programming/ContourTrees/tourtre-simple/cmake-build-debug/CMakeFiles/tourtre_simple.dir/src/ctComponent.c.o"
+  "/home/radu/Programming/ContourTrees/tourtre-simple/src/ctNode.c" "/home/radu/Programming/ContourTrees/tourtre-simple/cmake-build-debug/CMakeFiles/tourtre_simple.dir/src/ctNode.c.o"
+  "/home/radu/Programming/ContourTrees/tourtre-simple/src/ctNodeMap.c" "/home/radu/Programming/ContourTrees/tourtre-simple/cmake-build-debug/CMakeFiles/tourtre_simple.dir/src/ctNodeMap.c.o"
+  "/home/radu/Programming/ContourTrees/tourtre-simple/src/ctQueue.c" "/home/radu/Programming/ContourTrees/tourtre-simple/cmake-build-debug/CMakeFiles/tourtre_simple.dir/src/ctQueue.c.o"
+  "/home/radu/Programming/ContourTrees/tourtre-simple/src/tourtre.c" "/home/radu/Programming/ContourTrees/tourtre-simple/cmake-build-debug/CMakeFiles/tourtre_simple.dir/src/tourtre.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/radu/Programming/ContourTrees/tourtre-simple/examples/simple/src/Data.cpp" "/home/radu/Programming/ContourTrees/tourtre-simple/cmake-build-debug/CMakeFiles/tourtre_simple.dir/examples/simple/src/Data.cpp.o"
+  "/home/radu/Programming/ContourTrees/tourtre-simple/examples/simple/src/Mesh.cpp" "/home/radu/Programming/ContourTrees/tourtre-simple/cmake-build-debug/CMakeFiles/tourtre_simple.dir/examples/simple/src/Mesh.cpp.o"
+  "/home/radu/Programming/ContourTrees/tourtre-simple/examples/simple/src/main.cpp" "/home/radu/Programming/ContourTrees/tourtre-simple/cmake-build-debug/CMakeFiles/tourtre_simple.dir/examples/simple/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/tourtre_simple.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tourtre_simple.dir/examples/simple/src/Data.cpp.o"
+  "CMakeFiles/tourtre_simple.dir/examples/simple/src/main.cpp.o"
+  "CMakeFiles/tourtre_simple.dir/examples/simple/src/Mesh.cpp.o"
+  "CMakeFiles/tourtre_simple.dir/src/ctArc.c.o"
+  "CMakeFiles/tourtre_simple.dir/src/ctBranch.c.o"
+  "CMakeFiles/tourtre_simple.dir/src/ctComponent.c.o"
+  "CMakeFiles/tourtre_simple.dir/src/ctNode.c.o"
+  "CMakeFiles/tourtre_simple.dir/src/ctNodeMap.c.o"
+  "CMakeFiles/tourtre_simple.dir/src/ctQueue.c.o"
+  "CMakeFiles/tourtre_simple.dir/src/tourtre.c.o"
+  "tourtre_simple.pdb"
+  "tourtre_simple"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/tourtre_simple.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
